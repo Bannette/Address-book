d84c4fd8682efa5085d0ec693f368e4864e04e24
Bannette <banetaloo3@gmail.com>
2019-06-11 12:31:25 +0300

    added a prototype fullName

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
   }
+  Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  }
   // USER INTERFACE LOGIC
 $(document).ready(function() {
     $("form#new-contact").submit(function(event) {
@@ -13,12 +16,12 @@ $(document).ready(function() {
   
       var newContact = new Contact(inputtedFirstName, inputtedLastName);
   
-      $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
-  
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+      
       $("input#new-first-name").val("");
       $("input#new-last-name").val("");
     });
-    
+
     $(".contact").last().click(function() {
         $("#show-contact").show();
         $("#show-contact h2").text(newContact.firstName);
